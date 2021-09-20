3bb31e73af72314be8060621f8bfb6965506cc1f
dougpessoa <douglaspereira1@outlook.com>
2021-09-20 18:09:58 -0300

    1 file modified

--- Chart.tsx
+++ Chart.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  LineChart,
+  ResponsiveContainer,
   CartesianGrid,
   XAxis,
   YAxis,
@@ -16,39 +16,41 @@ import { data } from './data';
 const Chart = () => {
   return (
     <S.Wrapper>
-      <ComposedChart
-        width={730}
-        height={250}
-        data={data}
-        margin={{ top: 25, right: 30, left: 20, bottom: 5 }}
-      >
-        <defs>
-          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop
-              offset="0%"
-              stopColor="rgba(87, 112, 126, 0.3)"
-              stopOpacity={0.8}
-            />
-            <stop
-              offset="75.83%"
-              stopColor="rgba(87, 112, 126, 0.3)"
-              stopOpacity={0}
-            />
-          </linearGradient>
-        </defs>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="pv" stroke="#80B565" />
-        <Area
-          type="monotone"
-          dataKey="uv"
-          fill="url(#colorUv)"
-          stroke="#57707E"
-        />
-      </ComposedChart>
+      <ResponsiveContainer>
+        <ComposedChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 25, right: 30, left: 20, bottom: 5 }}
+        >
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop
+                offset="0%"
+                stopColor="rgba(87, 112, 126, 0.3)"
+                stopOpacity={0.8}
+              />
+              <stop
+                offset="75.83%"
+                stopColor="rgba(87, 112, 126, 0.3)"
+                stopOpacity={0}
+              />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#80B565" />
+          <Area
+            type="monotone"
+            dataKey="uv"
+            fill="url(#colorUv)"
+            stroke="#57707E"
+          />
+        </ComposedChart>
+      </ResponsiveContainer>
     </S.Wrapper>
   );
 };
